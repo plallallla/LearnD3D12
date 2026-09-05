aa5ca33c8344168e25ee57fd2db3e37167554e85
haifeng.peng <haifeng.peng@HAIFENGPENG-PC>
2026-09-05 16:37:38 +0800

    sync

---
+++ KillCow/main.cpp
@@ -0,0 +1,445 @@
+#include <cstdlib>
+#include <stdexcept>
+#include <string>
+
+#include <GlfwWindow.hpp>
+#include <Utility.hpp>
+
+
+#include <wrl/client.h>
+
+#include <dxgi1_6.h>
+#include <directx/d3d12sdklayers.h>
+#include <directx/d3dx12.h>
+
+#define FrameCount 2
+#define WIDTH 1280
+#define HEIGHT 720
+
+using namespace Microsoft::WRL;
+
+    struct Vertex
+    {
+        float position[3];
+        float color[4];
+    };
+
+inline void ThrowIfFailed(HRESULT hr)
+{
+    if (FAILED(hr))
+    {
+        throw std::runtime_error("Failed here!");
+    }
+}
+
+void enable_debug_layer(UINT& flag)
+{
+    ComPtr<ID3D12Debug> debug_controller;
+    if (FAILED(D3D12GetDebugInterface(IID_PPV_ARGS(&debug_controller)))) return;
+    debug_controller->EnableDebugLayer();
+    flag |= DXGI_CREATE_FACTORY_DEBUG;
+}
+
+ComPtr<IDXGIFactory4> create_factory(UINT flag)
+{
+    ComPtr<IDXGIFactory4> factory;
+    ThrowIfFailed(CreateDXGIFactory2(flag, IID_PPV_ARGS(&factory)));
+    return factory;
+}
+
+void GetHardwareAdapter(
+    IDXGIFactory1 *pFactory,
+    IDXGIAdapter1 **ppAdapter,
+    bool requestHighPerformanceAdapter = true)
+        {
+    *ppAdapter = nullptr;
+    ComPtr<IDXGIAdapter1> adapter;
+    ComPtr<IDXGIFactory6> factory6;
+    if (SUCCEEDED(pFactory->QueryInterface(IID_PPV_ARGS(&factory6))))
+    {
+        for (
+            UINT adapterIndex = 0;
+            SUCCEEDED(factory6->EnumAdapterByGpuPreference(
+                adapterIndex,
+                requestHighPerformanceAdapter == true ? DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE :
+                DXGI_GPU_PREFERENCE_UNSPECIFIED,
+                IID_PPV_ARGS(&adapter)));
+            ++adapterIndex)
+        {
+            DXGI_ADAPTER_DESC1 desc;
+            adapter->GetDesc1(&desc);
+            if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+            {
+                // Don't select the Basic Render Driver adapter.
+                // If you want a software adapter, pass in "/warp" on the command line.
+                continue;
+            }
+            // Check to see whether the adapter supports Direct3D 12, but don't create the
+            // actual device yet.
+            if (SUCCEEDED(D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
+            {
+                break;
+            }
+        }
+    }
+    if (adapter.Get() == nullptr)
+    {
+        for (UINT adapterIndex = 0; SUCCEEDED(pFactory->EnumAdapters1(adapterIndex, &adapter)); ++adapterIndex)
+        {
+            DXGI_ADAPTER_DESC1 desc;
+            adapter->GetDesc1(&desc);
+            if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+            {
+                // Don't select the Basic Render Driver adapter.
+                // If you want a software adapter, pass in "/warp" on the command line.
+                continue;
+            }
+            // Check to see whether the adapter supports Direct3D 12, but don't create the
+            // actual device yet.
+            if (SUCCEEDED(D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
+            {
+                break;
+            }
+        }
+    }
+    *ppAdapter = adapter.Detach();
+}
+
+ComPtr<ID3D12Device> create_device(UINT flag, ComPtr<IDXGIFactory4> factory)
+{
+    
+    ComPtr<ID3D12Device> device;
+    ThrowIfFailed(CreateDXGIFactory2(flag, IID_PPV_ARGS(&factory)));
+    ComPtr<IDXGIAdapter1> hardwareAdapter;
+    GetHardwareAdapter(factory.Get(), &hardwareAdapter);
+    ThrowIfFailed(D3D12CreateDevice(
+        hardwareAdapter.Get(),
+        D3D_FEATURE_LEVEL_11_0,
+        IID_PPV_ARGS(&device)
+    ));
+    return device;
+}
+
+
+ComPtr<ID3D12CommandQueue> create_command_queue(ComPtr<ID3D12Device> device)
+{
+    ComPtr<ID3D12CommandQueue> command_queue;
+    D3D12_COMMAND_QUEUE_DESC desc{};
+    desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+    ThrowIfFailed(device->CreateCommandQueue(&desc, IID_PPV_ARGS(&command_queue)));
+    return command_queue;
+}
+
+
+ComPtr<IDXGISwapChain3> create_swap_chain(HWND h, ComPtr<IDXGIFactory4> factory, ComPtr<ID3D12CommandQueue> command_queue)
+{
+    ComPtr<IDXGISwapChain3> ret_sc3;
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+    swapChainDesc.Width = WIDTH;
+    swapChainDesc.Height = HEIGHT;
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.SampleDesc.Count = 1;
+    swapChainDesc.SampleDesc.Quality = 0;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.BufferCount = FrameCount;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
+    swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+    swapChainDesc.Flags = 0;
+    ComPtr<IDXGISwapChain1> swapChain;
+    ThrowIfFailed(factory->CreateSwapChainForHwnd(
+        command_queue.Get(),        // Swap chain needs the queue so that it can force a flush on it.
+        h,
+        &swapChainDesc,
+        nullptr,
+        nullptr,
+        &swapChain
+        ));
+    ThrowIfFailed(factory->MakeWindowAssociation(h, DXGI_MWA_NO_ALT_ENTER));    
+    ThrowIfFailed(swapChain.As(&ret_sc3));
+    return ret_sc3;
+}
+
+ComPtr<ID3D12DescriptorHeap> create_descriptor_heap(ComPtr<ID3D12Device> device, UINT ct, D3D12_DESCRIPTOR_HEAP_TYPE type, bool shader_visible)
+{
+        ComPtr<ID3D12DescriptorHeap> rtv_heap;
+        D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc{};
+        rtvHeapDesc.NumDescriptors = ct;
+        rtvHeapDesc.Type = type;
+        rtvHeapDesc.Flags = shader_visible ? D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE : D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        // rtvHeapDesc.NodeMask = 0;
+        ThrowIfFailed(device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtv_heap)));
+        return rtv_heap;
+}
+
+struct D3D12Resource
+{
+    ComPtr<ID3D12Resource> m_resource;
+
+};
+
+struct D3D12RenderPass
+{
+    
+    void set()
+    {
+
+    }
+    void render()
+    {
+
+    }
+};
+
+struct D3D12Context
+{
+    // gloable context
+    CD3DX12_VIEWPORT m_viewport;
+    CD3DX12_RECT m_scissorRect;
+    ComPtr<IDXGISwapChain3> m_swapChain;
+    ComPtr<ID3D12Device> m_device;
+    ComPtr<IDXGIFactory4> m_factory;
+    ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
+    ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+    ComPtr<ID3D12CommandQueue> m_commandQueue;
+    // memory
+    ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+    // pipeline
+    ComPtr<ID3D12RootSignature> m_rootSignature;
+    ComPtr<ID3D12PipelineState> m_pipelineState;
+    ComPtr<ID3D12GraphicsCommandList> m_commandList;
+    UINT m_rtvDescriptorSize;
+    UINT m_frameIndex;
+    
+    // App resources.
+    ComPtr<ID3D12Resource> m_vertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+
+    // Synchronization objects.
+    HANDLE m_fenceEvent;
+    ComPtr<ID3D12Fence> m_fence;
+    UINT64 m_fenceValue;
+
+    void WaitForPreviousFrame()
+    {
+        // Signal and increment the fence value.
+        const UINT64 fence = m_fenceValue;
+        ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), fence));
+        m_fenceValue++;
+
+        // Wait until the previous frame is finished.
+        if (m_fence->GetCompletedValue() < fence)
+        {
+            ThrowIfFailed(m_fence->SetEventOnCompletion(fence, m_fenceEvent));
+            WaitForSingleObject(m_fenceEvent, INFINITE);
+        }
+
+        m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+    }
+
+    D3D12Context(HWND h)
+    {
+        UINT dxgiFactoryFlags = 0;
+#if defined(_DEBUG)
+            enable_debug_layer(dxgiFactoryFlags);
+#endif
+
+        // factory
+        m_factory = create_factory(dxgiFactoryFlags);
+
+        // device
+        m_device = create_device(dxgiFactoryFlags, m_factory);
+
+        // command queue
+        m_commandQueue = create_command_queue(m_device);
+
+        // swapchain
+        m_swapChain = create_swap_chain(h, m_factory, m_commandQueue);
+
+        // rtv heap
+        m_rtvHeap = create_descriptor_heap(m_device, FrameCount, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, D3D12_DESCRIPTOR_HEAP_FLAG_NONE);
+
+        m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+        m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+        // frame resource
+        CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle{m_rtvHeap->GetCPUDescriptorHandleForHeapStart()};
+        for (UINT n = 0; n < FrameCount; ++n)
+        {
+            ThrowIfFailed(m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n])));
+            m_device->CreateRenderTargetView(m_renderTargets[n].Get(), nullptr, rtvHandle);
+            rtvHandle.Offset(1, m_rtvDescriptorSize);
+        }
+
+        // command allocator
+        ThrowIfFailed(m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator)));
+
+
+        CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
+        rootSignatureDesc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+        // empty root signature
+        ComPtr<ID3DBlob> signature;
+        ComPtr<ID3DBlob> error;
+        ThrowIfFailed(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error));
+        ThrowIfFailed(m_device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_rootSignature)));
+
+        // shader
+        auto vertexShaderData = ReadCSO(std::string{SHADER_DIR} + "/triangle_VSMain.cso");
+        auto pixelShaderData  = ReadCSO(std::string{SHADER_DIR} + "/triangle_PSMain.cso");
+        UINT8* pVertexShaderData = vertexShaderData.data();
+        UINT8* pPixelShaderData = pixelShaderData.data();
+        UINT vertexShaderDataLength = static_cast<UINT>(vertexShaderData.size());
+        UINT pixelShaderDataLength = static_cast<UINT>(pixelShaderData.size());
+
+        // layout
+        D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
+        {
+            { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+            { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+        };
+
+        // pso
+        D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+        psoDesc.InputLayout = { inputElementDescs, _countof(inputElementDescs) };
+        psoDesc.pRootSignature = m_rootSignature.Get();
+        psoDesc.VS = CD3DX12_SHADER_BYTECODE(pVertexShaderData, vertexShaderDataLength);
+        psoDesc.PS = CD3DX12_SHADER_BYTECODE(pPixelShaderData, pixelShaderDataLength);
+        psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+        psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+        psoDesc.DepthStencilState.DepthEnable = FALSE;
+        psoDesc.DepthStencilState.StencilEnable = FALSE;
+        psoDesc.SampleMask = UINT_MAX;
+        psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+        psoDesc.NumRenderTargets = 1;
+        psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+        psoDesc.SampleDesc.Count = 1;
+        ThrowIfFailed(m_device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&m_pipelineState)));
+
+
+        m_viewport = CD3DX12_VIEWPORT(0.0f, 0.0f, static_cast<float>(WIDTH), static_cast<float>(HEIGHT));
+        m_scissorRect = CD3DX12_RECT(0, 0, WIDTH, HEIGHT);
+
+        // command list.
+        ThrowIfFailed(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), m_pipelineState.Get(), IID_PPV_ARGS(&m_commandList)));
+        ThrowIfFailed(m_commandList->Close());
+
+        // mesh
+        auto m_aspectRatio = static_cast<float>(WIDTH) / static_cast<float>(HEIGHT);
+        Vertex triangleVertices[] =
+        {
+            { { 0.0f, 0.25f * m_aspectRatio, 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+            { { 0.25f, -0.25f * m_aspectRatio, 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+            { { -0.25f, -0.25f * m_aspectRatio, 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }
+        };
+
+        CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_UPLOAD);
+        auto bufferDesc = CD3DX12_RESOURCE_DESC::Buffer(sizeof(triangleVertices));
+        const UINT vertexBufferSize = sizeof(triangleVertices);
+        ThrowIfFailed(m_device->CreateCommittedResource(
+            &heapProps,
+            D3D12_HEAP_FLAG_NONE,
+            &bufferDesc,
+            D3D12_RESOURCE_STATE_GENERIC_READ,
+            nullptr,
+            IID_PPV_ARGS(&m_vertexBuffer)));
+
+        // Copy the triangle data to the vertex buffer.
+        UINT8* pVertexDataBegin;
+        CD3DX12_RANGE readRange(0, 0);        // We do not intend to read from this resource on the CPU.
+        ThrowIfFailed(m_vertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)));
+        memcpy(pVertexDataBegin, triangleVertices, sizeof(triangleVertices));
+        m_vertexBuffer->Unmap(0, nullptr);
+
+        // Initialize the vertex buffer view.
+        m_vertexBufferView.BufferLocation = m_vertexBuffer->GetGPUVirtualAddress();
+        m_vertexBufferView.StrideInBytes = sizeof(Vertex);
+        m_vertexBufferView.SizeInBytes = vertexBufferSize;
+
+
+        // sync
+        ThrowIfFailed(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)));
+        m_fenceValue = 1;
+        m_fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+        if (m_fenceEvent == nullptr)
+        {
+            ThrowIfFailed(HRESULT_FROM_WIN32(GetLastError()));
+        }
+        WaitForPreviousFrame();
+
+    }
+
+    void populate_command_list()
+    {
+        // Command list allocators can only be reset when the associated command lists have finished execution on the GPU; apps should use fences to determine GPU execution progress.
+        ThrowIfFailed(m_commandAllocator->Reset());
+
+        // However, when ExecuteCommandList() is called on a particular command list, that command list can then be reset at any time and must be before re-recording.
+        ThrowIfFailed(m_commandList->Reset(m_commandAllocator.Get(), m_pipelineState.Get()));
+
+        // Set necessary state.
+        m_commandList->SetGraphicsRootSignature(m_rootSignature.Get());
+        m_commandList->RSSetViewports(1, &m_viewport);
+        m_commandList->RSSetScissorRects(1, &m_scissorRect);
+
+        // Indicate that the back buffer will be used as a render target.
+        const auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(
+            m_renderTargets[m_frameIndex].Get(),
+            D3D12_RESOURCE_STATE_PRESENT,
+            D3D12_RESOURCE_STATE_RENDER_TARGET);
+        m_commandList->ResourceBarrier(1, &barrier);
+
+        CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle{m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), (INT)m_frameIndex, m_rtvDescriptorSize};
+        m_commandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
+
+        // Record commands.
+        const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+        m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+        m_commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+        m_commandList->IASetVertexBuffers(0, 1, &m_vertexBufferView);
+        m_commandList->DrawInstanced(3, 1, 0, 0);
+
+        // Indicate that the back buffer will now be used to present.
+        const auto barrier2 = CD3DX12_RESOURCE_BARRIER::Transition(
+            m_renderTargets[m_frameIndex].Get(),
+            D3D12_RESOURCE_STATE_RENDER_TARGET,
+            D3D12_RESOURCE_STATE_PRESENT);
+        m_commandList->ResourceBarrier(1, &barrier2);
+
+        ThrowIfFailed(m_commandList->Close());
+    }
+
+    void render()
+    {
+        populate_command_list();
+
+        // Execute the command list.
+        ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
+        m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+
+        // Present the frame.
+        ThrowIfFailed(m_swapChain->Present(1, 0));
+
+        WaitForPreviousFrame();
+    }
+
+
+
+};
+
+
+
+
+int main()
+{
+    GlfwWindow window{WIDTH, HEIGHT, "test"};
+    D3D12Context device{ window._hwnd };
+    while (!glfwWindowShouldClose(window._window))
+    {
+        device.render();
+        glfwPollEvents();
+    }
+    return 0;
+}
+
